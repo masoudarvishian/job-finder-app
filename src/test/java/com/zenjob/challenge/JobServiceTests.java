8d14eacf291f391e86409c3c7595c2e168da71e5
masoudarvishian <masoudarvishian@gmail.com>
2024-05-09 20:45:13 +0200

    Added job_should_have_at_least_one_shift unit test

--- src/test/java/com/zenjob/challenge/JobServiceTests.java
+++ src/test/java/com/zenjob/challenge/JobServiceTests.java
@@ -1,5 +1,6 @@
 package com.zenjob.challenge;
 
+import com.zenjob.challenge.entity.Job;
 import com.zenjob.challenge.service.IJobService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,13 +17,22 @@ public class JobServiceTests {
     private IJobService jobService;
 
     @Test
+    public void job_should_have_at_least_one_shift() {
+        LocalDate startDate = LocalDate.now();
+        LocalDate endDate = LocalDate.now().plusDays(1);
+
+        Job job = jobService.createJob(UUID.randomUUID(), startDate, endDate);
+
+        Assertions.assertEquals(1, job.getShifts().size());
+    }
+
+    @Test
     public void start_date_cannot_be_in_the_past() {
         LocalDate startDate = LocalDate.now().minusDays(10);
         LocalDate endDate = LocalDate.now().plusDays(2);
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            jobService.createJob(UUID.randomUUID(), startDate, endDate);
-        });
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                jobService.createJob(UUID.randomUUID(), startDate, endDate));
     }
 
 }
